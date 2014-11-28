0125beac99e530fd116aba77c2ad6215e67f3404
chenshaonian <594171643@qq.com>
2014-11-28 01:18:29 -0800

    update lesson6 but not finish yet. un load chaijs module...

---
+++ node-lessons/lesson6/f2e/tests.js
@@ -0,0 +1,7 @@
+var chai = window.mocha.require('chai');
+var should = chai.should();
+describe('simple test', function () {
+  it('should equal 0 when n === 0', function () {
+    window.fibonacci(0).should.equal(0);
+  });
+});
